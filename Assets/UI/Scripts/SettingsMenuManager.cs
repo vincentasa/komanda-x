8a0318df33017932e3cb8bfd19fb86a18ab24690
vincentasa <vincentas.augustas@gmail.com>
2024-05-28 18:52:54 +0300

    added sounds and etc

--- Assets/UI/Scripts/SettingsMenuManager.cs
+++ Assets/UI/Scripts/SettingsMenuManager.cs
@@ -2,10 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
+using UnityEngine.Audio;
 
 public class SettingsMenuManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public TMP_Dropdown graphicsDropdown;
+    public Slider masterVol, musicVol, sfxVol;
+    public AudioMixer mainMixer;
+
+
+
+    public void ChangeGraphicsQuality()
+    {
+        QualitySettings.SetQualityLevel(graphicsDropdown.value);
+    }
+
+    public void ChangeMasterVolume()
+    {
+        mainMixer.SetFloat("MasterVol", masterVol.value);
+    }
+    public void ChangeMusicVolume()
+    {
+        mainMixer.SetFloat("MusicVol", musicVol.value);
+    }
+    public void ChangeSFXVolume()
+    {
+        mainMixer.SetFloat("SFXVol", sfxVol.value);
+    }
+
+
+
     void Start()
     {
         
